35b2aad43be1620300d5d3dfb48743a2ad3e2f13
dimafarer <saasbook@saasbook.(none)>
2013-11-14 20:08:08 -0800

    added create controller function and flash message, and view flash message to all view application view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,10 @@ class MoviesController < ApplicationController
 	def new
 
 	end
+
+	def create
+		@movie = Movie.create!(params[:movie])
+		flash[:notice] = "#{@movie.title} was successfully created."
+		redirect_to movies_path
+	end
 end
