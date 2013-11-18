9613b49dbdd9ec0826b5757cee5db4e3ef88cb13
dimafarer <saasbook@saasbook.(none)>
2013-11-18 12:36:48 -0800

    added hadleing of xml version html views, add pattern to igonre "~" files, next will delete all "~" files

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,13 +26,16 @@ class MoviesController < ApplicationController
 		@movie = Movie.find params[:id]
 		@movie.update_attributes!(params[:movie])
 		flash[:notice] = "#{@movie.title} was successfully updated."
-		redirect_to movie_path(@movie)
+		respond_to do |client_wants|
+			client_wants.html { redirect_to movie_path(@movie) }
+			client_wants.xml  { rend :xml => @movie.to_xml     }
+		end
 	end
 
-def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  flash[:notice] = "Movie '#{@movie.title}' deleted."
-  redirect_to movies_path
-end
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:notice] = "Movie '#{@movie.title}' deleted."
+		redirect_to movies_path
+	end
 end
